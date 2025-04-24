509b1cb925abb62c3dc2341e462ec6c5e60b099b
ko-yamada@vanfu.co.jp <73684559+ko-yamada@users.noreply.github.com>
2025-04-24 11:37:15 +0900

    コマンドライン引数からsleep時間を取得する機能を追加

--- sample_code/parallel_run.go
+++ sample_code/parallel_run.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	// ランダムな秒数でsleepする
 	println("Build started")
+
+	// コマンドライン引数からsleep時間を取得
 	sleepTime, _ := strconv.Atoi(os.Args[2])
 	time.Sleep(time.Duration(sleepTime) * time.Second)
 	println(os.Args[1], " successfully")
